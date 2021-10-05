ff98dbb3ecbf51ee7e53d4d793997ead8e45339c
Saltcult <qubakuba@gmail.com>
2021-10-05 11:55:30 +0000

    more

--- src/index.js
+++ src/index.js
@@ -1,7 +1,62 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 
+const aSimpleElement = <h1>Hello all!</h1>;
 
-const helloWorld = <h1>Hello World</h1>;
+function StatelessComponent() {
+  return <h1>Hello all from stateless!</h1>;
+}
 
-ReactDOM.render(helloWorld, document.getElementById("root"));
+class StatefulComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "World",
+      date: new Date(),
+    };
+  }
+
+  handleChange(event) {
+    const name = event.target.value;
+    this.setState({ name: name });
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  componentDidMount() {
+    console.log("Mounted!");
+    setInterval(() => {
+      
+      this.tick();
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Hello {this.state.name}!</h1>
+        <h1>It's {this.state.date.toLocaleTimeString()}</h1>
+        <input
+          type="text"
+          onChange={(event) => {
+            
+            this.handleChange(event);
+          }}
+        />
+      </>
+    );
+  }
+}
+
+const app = (
+  <>
+    
+
+    <StatefulComponent />
+  </>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
